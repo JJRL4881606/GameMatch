dc83a8775e533e60bacbf1f1f2e8c975fc020ae5
Jacobo Rodríguez <alfonsojacoborg@gmail.com>
2026-05-10 12:22:47 -0700

    Se corrigio el path de los jars

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,7 @@ package main;
 import javax.swing.UIManager;
 import javax.swing.plaf.FontUIResource;
 
-import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
-
 import utils.AppFont;
 import views.MainWindow;
 
@@ -13,7 +11,7 @@ public class Main {
 
     @SuppressWarnings("unused")
 	public static void main(String[] args) {
-        FlatLightLaf.setup();
+    	FlatLightLaf.setup();
         UIManager.put("defaultFont", new FontUIResource(AppFont.normal()));
         MainWindow ventanita = new MainWindow();
     }
